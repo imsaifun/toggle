62fecb6cfa06bc017afede3f40b52dba8641d4bc
Golam Mostafa <89465189+imsaifun@users.noreply.github.com>
2021-09-03 06:37:57 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+
+function App() {
+    const [isToggled, setToggled] = useState(false);
+    const toggleTrueFalse = () => setToggled(!isToggled);
+  return (
+    <div className="App">
+      <button onClick={toggleTrueFalse}>
+         Toggle me
+        </button>
+
+        <div className={isToggled? "text red" : " text"}> Hello Toggle</div>
+    </div>
+  );
+}
+
+export default App;
